b31fe48ede2267081cf3784f1f0a4034f326e43d
Saad <sbenkirane.mis2016@student.ie.edu>
2019-04-11 12:05:23 +0200

    Final exam debugging: Correct code

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,30 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replace(".", "/");
 
         Random random = new Random();
         StringBuffer word = null;
 
-        switch (random.nextInt(2)) {
+        int randomNumber = random.nextInt(3);
+
+        switch (randomNumber) {
             case 0:
-                word = new StringBuffer('Y');
+                word = new StringBuffer("Y");
+                break;
             case 1:
-                word = new StringBuffer('F');
+                word = new StringBuffer("F");
+                break;
             case 2:
-                word = new StringBuffer('T');
+                word = new StringBuffer("T");
+                break;
         }
 
-        word.append('o');
-        word.append('u');
-        word.append('r');
+        word.append("o");
+        word.append("u");
+        word.append("r");
 
         System.out.println(word + " " + formattedEmailAddress);
     }
